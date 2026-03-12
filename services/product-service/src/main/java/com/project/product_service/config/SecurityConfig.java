1f9b97dbf69171294c5359ce7533c9efa07c16d4
dHRvBiyAn1 <dhruvbiyani022@gmail.com>
2026-03-12 23:38:07 +0530

    feat: product-service working - connected to mongodb

--- services/product-service/src/main/java/com/project/product_service/config/SecurityConfig.java
+++ services/product-service/src/main/java/com/project/product_service/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.project.product_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.project.product_service.Security.HeaderAuthenticationFilter;
 
@@ -20,21 +22,21 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final HeaderAuthenticationFilter headerAuthenticationFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/api/v1/products/**", "/api/v1/categories/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .httpBasic(httpBasic -> httpBasic.disable());
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/v1/products/**", "/api/v1/categories/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(httpBasic -> httpBasic.disable());
         return http.build();
     }
 }
