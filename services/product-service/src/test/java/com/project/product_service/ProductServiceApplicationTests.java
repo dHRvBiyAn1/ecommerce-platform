a030ddfc369825713c40f12f506a3daf5fb039a3
dHRvBiyAn1 <dhruvbiyani022@gmail.com>
2026-05-17 02:44:52 +0530

    fix: mock ProductSearchRepository in context test to avoid ES connection

--- services/product-service/src/test/java/com/project/product_service/ProductServiceApplicationTests.java
+++ services/product-service/src/test/java/com/project/product_service/ProductServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.project.product_service;
 
+import com.project.product_service.search.ProductSearchRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
+
+	@MockitoBean
+	private ProductSearchRepository productSearchRepository;
 
 	@Test
 	void contextLoads() {
